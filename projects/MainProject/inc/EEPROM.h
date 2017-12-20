36feb1bb198362184b5bc9d5ee2a3c4044c989f5
bastiaan metz <bastiaanmetz@gmail.com>
2017-12-20 09:35:46 +0100

    commit3
    haha

--- projects/MainProject/inc/EEPROM.h
+++ projects/MainProject/inc/EEPROM.h
@@ -15,7 +15,30 @@
 extern "C" {
 #endif
 
+#include "stm32f0xx.h"
+#include "helper.h"
+	
+//device 7-bits address assuming A0=A1=A2=0
+#define EEPROM_ADDR               0xA0
+	
+// Maximum timeout value for waiting loops
+// This timeout is not based on an accurate timing value. It just guarantess
+// that the program will not get stuck in an endless loop whenever there is an
+// error in the I2C communication
+#define EEPROM_TIMEOUT            ((uint32_t)(0x00001000))
+
+// EEPROM communication status information
+#define EEPROM_COMM_OK            (0x0)
+#define EEPROM_COMM_ERROR         (0x1)
+	
 /* Public functions --------------------------------------------------------*/
+void    					I2C_Setup( void );
+void    					SE24LC512_Clear(void);
+initPage 					EEPROM_getInitPage(void); 
+void 							EEPROM_setInitPage(initPage page);
+void 							EEPROM_setRevalidationData(uint16_t addr, revalidationData data);
+revalidationData 	EEPROM_getRevalidationData(uint16_t addr);
+
 /* Public variables --------------------------------------------------------*/
 
 #ifdef __cplusplus
--- projects/MainProject/src/main.c
+++ projects/MainProject/src/main.c
@@ -11,8 +11,15 @@
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f0xx_it.h"
 #include "sendReceiveUART.h"
+#include "helper.h"
 
 int main(){
 	USART_init();
-	USART_putc('h');
+	USART_putc('1');
+	Delay(SystemCoreClock / 8 / 1);
+  USART_putc('2');
+	Delay(SystemCoreClock / 8 / 1);
+	USART_putc('1');
+	Delay2(2000);
+	USART_putc('2');
 }
